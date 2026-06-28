e8ecf970df4c6744f7cb9bb9120342d8dc2b59f6
Voyrox <ewen@macculloch.net>
2026-06-28 17:20:20 +1000

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/ewen/Documents/GitHub/Air-Gapped Update Distribution System
-# Build directory: /mnt/c/Users/ewen/Documents/GitHub/Air-Gapped Update Distribution System/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/c/Users/ewen/Documents/GitHub/Air-Gapped Update Distribution System/build/blackbox_test[1]_include.cmake")
